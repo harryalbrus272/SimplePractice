3e11f7ca7ab9c515c9b7b03fbc35f531cca849a2
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-12 23:31:36 +0530

    api call to the shop and dynamic routing

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Shop = () => {
-    return (
-        <div>
-            <h2>Shop</h2>
-        </div>
-    )
-}
+  const [items, setItems] = useState([]);
+  const [isloaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  const fetchItems = async () => {
+    const { data } = await axios.get("https://fakestoreapi.com/products");
+    await setItems(data);
+    setIsLoaded(true);
+    console.log(data);
+  };
+  return (
+    <div>
+      {isloaded && items.map((item) => <h5 key={item.id}><Link to={`/shop/${item.id}`}>{item.title}</Link></h5>)}
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;

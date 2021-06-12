1e058fda6c452391047ad6486e7d22bd533f5928
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-12 22:35:21 +0530

    finish adding links to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <nav>
-            <h2>Logo</h2>
-            <ul className="nav-links">
-                <li>About</li>
-                <li>Shop</li>
-                <li>Box</li>
-            </ul>
-        </nav>
-    )
-}
+  const styleNav = {
+    color: "white",
+    textDecoration: "none",
+  };
+  return (
+    <nav>
+      <Link style={styleNav} to="/about">
+        <h2>Logo</h2>
+      </Link>
+      <ul className="nav-links">
+        <Link style={styleNav} to="/about">
+          <li>About</li>
+        </Link>
+        <Link style={styleNav} to="/shop">
+          <li>Shop</li>
+        </Link>
+        <Link style={styleNav} to="/box">
+          <li>Box</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;

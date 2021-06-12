beba7eaf333ab93cb0cfb34635f765f76480f10b
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-12 21:12:28 +0530

    initial commit with installed libraries

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <h2>Logo</h2>
+            <ul className="nav-links">
+                <li>About</li>
+                <li>Shop</li>
+                <li>Box</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Nav from "./Nav";
+import Box from "./Box";
+import About from "./About";
+import Shop from "./Shop";
+export { Nav, Box, About, Shop };

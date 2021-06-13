f397c2804107280af5a93b8cb59d8a71800e484c
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-13 15:12:09 +0530

    withStyles and theming applied

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Paper,
@@ -6,6 +6,11 @@ import {
   Checkbox,
   FormGroup,
   FormControlLabel,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  RadioGroup,
+  Radio,
 } from "@material-ui/core";
 import {
   makeStyles,
@@ -20,6 +25,34 @@ import { purple, green } from "@material-ui/core/colors";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 const Box = () => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState("Choose wisely");
+  const [state, setState] = useState({
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedG: true,
+  });
+  const handleRadioChange = (event) => {
+    setValue(event.target.value);
+    setError(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (value === "best") {
+      setHelperText("You got it!");
+      setError(false);
+    } else if (value === "worst") {
+      setHelperText("Sorry, wrong answer!");
+      setError(true);
+    } else {
+      setHelperText("Please select an option.");
+      setError(true);
+    }
+  };
   const ColorButton = withStyles((theme) => ({
     root: {
       color: theme.palette.getContrastText(purple[500]),
@@ -47,6 +80,15 @@ const Box = () => {
       {...props}
     />
   ));
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  console.log({ state, value });
+  console.log({ value });
+  console.log({ error });
+
   const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
@@ -62,6 +104,12 @@ const Box = () => {
     },
     cloudButtonIcon: {
       color: "#e7a6d9",
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+    buttonRadio: {
+      margin: theme.spacing(1, 1, 0, 0),
     },
   }));
   const classes = useStyles();
@@ -115,7 +163,13 @@ const Box = () => {
             xs=6 sm=3
             <FormGroup row>
               <FormControlLabel
-                control={<GreenCheckbox />}
+                control={
+                  <GreenCheckbox
+                    checked={state.checkedG}
+                    onChange={handleChange}
+                    name="checkedG"
+                  />
+                }
                 label="Custom icon"
               />
             </FormGroup>
@@ -128,6 +182,41 @@ const Box = () => {
           <Paper className={classes.paper}>xs=6 sm=3</Paper>
         </Grid>
       </Grid>
+      <form onSubmit={handleSubmit}>
+        <FormControl
+          component="fieldset"
+          error={error}
+          className={classes.formControl}
+        >
+          <FormLabel component="legend">Pop quiz: Material-UI is...</FormLabel>
+          <RadioGroup
+            aria-label="quiz"
+            name="quiz"
+            value={value}
+            onChange={handleRadioChange}
+          >
+            <FormControlLabel
+              value="best"
+              control={<Radio />}
+              label="The best!"
+            />
+            <FormControlLabel
+              value="worst"
+              control={<Radio />}
+              label="The worst."
+            />
+          </RadioGroup>
+          <FormHelperText>{helperText}</FormHelperText>
+          <Button
+            type="submit"
+            variant="outlined"
+            color="primary"
+            className={classes.buttonRadio}
+          >
+            Check Answer
+          </Button>
+        </FormControl>
+      </form>
     </div>
   );
 };
